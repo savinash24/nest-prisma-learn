65b93318420caced1dc6f7e641137790511eb402
chattisanjana <avinash.nani789@gmail.com>
2024-08-09 16:22:59 +0530

    removed unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,26 +2,15 @@ import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-// import { PrismaService } from './common/prisma/prisma.service';
-// import { PrismaModule } from './common/prisma/prisma.module';
-import { ConfigModule } from '@nestjs/config';
 import { DatabaseMiddleware } from './middleware/database.middleware';
 
 @Module({
-  imports: [
-    // ConfigModule.forRoot({
-    //   isGlobal: true, // Ensure that environment variables are available globally
-    // }),
-    // ,
-    UserModule,
-  ],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule{
+export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(DatabaseMiddleware)
-      .forRoutes('*'); // Apply to all routes
+    consumer.apply(DatabaseMiddleware).forRoutes('*'); // Apply to all routes
   }
 }
