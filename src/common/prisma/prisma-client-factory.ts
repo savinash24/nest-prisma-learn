5eb0fb786e7b0a30328788795fb06874b2d287df
chattisanjana <avinash.nani789@gmail.com>
2024-08-09 11:57:02 +0530

    logging enabled

--- src/common/prisma/prisma-client-factory.ts
+++ src/common/prisma/prisma-client-factory.ts
@@ -1,5 +1,7 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+
 const prismaClientCache: { [key: string]: PrismaClient } = {};
+
 export const getPrismaClient = (databaseName: string): PrismaClient => {
   const databaseUrl = `${process.env.DATABASE_URL}/${databaseName}`;
 
@@ -10,7 +12,32 @@ export const getPrismaClient = (databaseName: string): PrismaClient => {
           url: databaseUrl,
         },
       },
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'info' },
+        { emit: 'event', level: 'warn' },
+        { emit: 'event', level: 'error' },
+      ],
     });
+
+    client.$on('query', (e) => {
+      console.log(`Query: ${e.query}`);
+      console.log(`Params: ${e.params}`);
+      console.log(`Duration: ${e.duration}ms`);
+    });
+
+    client.$on('info', (e) => {
+      console.log(`Info: ${e.message}`);
+    });
+
+    client.$on('warn', (e) => {
+      console.log(`Warn: ${e.message}`);
+    });
+
+    client.$on('error', (e) => {
+      console.log(`Error: ${e.message}`);
+    });
+
     prismaClientCache[databaseUrl] = client;
   }
   return prismaClientCache[databaseUrl];
